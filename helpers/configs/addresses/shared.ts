68647685b91a132b139460e07ec62e62f60dbc36
Joaquin Failo <73504599+Joafai@users.noreply.github.com>
2026-05-05 03:05:50 -0300

    Update Aave Permission Book

--- helpers/configs/addresses/shared.ts
+++ helpers/configs/addresses/shared.ts
@@ -20,6 +20,32 @@ export const SHARED_ADDRESSES: Record<string, string> = {
 
   // MASIV Safe - emission admin
   '0xdef1FA4CEfe67365ba046a7C630D6B885298E210': 'MASIV Safe',
+
+  // Aave Protocol Embassy
+  '0xAA43203167317DeeF8288095C44b84a686918d2E': 'APE Assets',
+  '0xa9e777D56C0Ad861f6a03967E080e767ad8D39b6': 'APE Voting',
+
+  // Aave Liquidity Committee
+  '0xA1c93D2687f7014Aaf588c764E3Ce80aF016229b': 'ALC',
+  '0xAAB6f926DCDaE536F54ce58478Dbc1a0d0f98871': 'ALC Incentive',
+  '0xAA484Ba6a7f51f00A3f82a11e73b741AE1dEAB58': 'ALC Voting',
+
+  // Frontier
+  '0xCDb4fA6ba08bF1FB7Aa9fDf6002E78EDc431a642': 'Frontier',
+
+  // MERIT & AHAB
+  '0xAA870e4B82deaDa3727235f34183Ec9B728714C8': 'Merit Incentives',
+  '0xAA2461f0f0A3dE5fEAF3273eAe16DEF861cf594e': 'Ahab',
+
+  // Aave Finance Committee
+  '0xAA12BAd4a501d45A5b771e49C2Fd415BA8BFc79d': 'AFC CEX Earn',
+  '0xaa7A1910BA79B6A2E385ebA26185aA2dCB9B8eAd': 'AFC CEX Earn Incentive',
+
+  // Aave v4 Security
+  '0xAAf400e4Bbc38B5E2136C1a36946Bf841A357307': 'Aave v4 Security SAFE',
+
+  // Aave Automation Cancel SAFE
+  '0x441E4053fDDF1e1a77a39d00309Af389096d4124': 'Aave Automation Cancel SAFE',
 };
 
 /**
